ec9a6d81eb29bd0712008538f2fbd23d78fdcb1d
20jota02 <juananacona04@gmail.com>
2023-03-08 09:25:07 -0500

    inicio de  Proyecto

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const conectarDB = require('./src/config/connection')
+const dotenv = require ('dotenv');
+const cors = require ("cors")
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors())
+dotenv.config();
+app.use(express.json());
+
+
+conectarDB();
+
+app.listen(PORT, (err) => {
+    if(err){
+        console.log(`server error: ${err.message}`);
+    } else{
+        console.log(`server is running on port ${PORT}`)
+    }
+
+})
